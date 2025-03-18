285f0b0376dfc151cee1603e57b3e367458aa035
JorshSlimming <benjahc1905@gmail.com>
2025-03-18 17:14:22 -0300

    main

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import ListHeader from "./components/ListHeader"
+
+function App() {
+  return (
+    <div className = "app">
+      <ListHeader listName = {'Holiday tick list'}>
+
+      </ListHeader>
+    </div>
+  );
+}
+
+export default App;
